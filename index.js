8bd45b4d841a8ba2ee7503c6036f7162833e1f40
sxbates <86023494+sxbates@users.noreply.github.com>
2021-10-02 14:05:16 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+function App(){
+    const [todos, setTodos] = React.useState([
+      {
+        text: 'learn react',
+        isCompleted: false,
+      },
+      {
+        text: 'meet friend for lunch',
+        isCompleted: false,
+      },
+      {
+        text: 'build todo app',
+        isCompleted: false,
+      }        
+    ]);
+  
+    const addTodo = text => {
+      const newTodos = [...todos, {text, isCompleted:false}];
+      setTodos(newTodos);
+    }
+    const removeTodo = index => {
+      let temp = [...todos];    
+      temp.splice(index, 1);
+      setTodos(temp);
+    }
+  
+    return(
+        <div className="app">
+            <div className="todo-list">
+              {todos.map((todo, i) => 
+              <Todo index = {i} id={i} todo={todo} remove={removeTodo}/>)}
+              <TodoForm addTodo={addTodo} />
+             
+              <div>Scott has {todos.length} things he still needs to do today...</div>
+        </div>
+      </div>
+      
+    );
+  }
+  
+  ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+  );
